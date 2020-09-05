40f564df61194b27e6c8db0da4c84e10002c1f23
Pawel Walat <pawel@walat.net>
2020-09-05 13:09:33 +0200

    Fix

--- taskmanager/cli.py
+++ taskmanager/cli.py
@@ -27,6 +27,7 @@ def cli():
     manage.add_argument('-a', "--add", dest='task_definition', action='store', help='Add task')    
     manage.add_argument('-Tn', "--task-name", dest='task_name', default="", action='store', help='')   
     manage.add_argument('-Tt', "--task-type", dest='task_type', default="python", action='store', help='')   
+    manage.add_argument('-Tp', "--task-parameter", dest='task_parameter', default="", action='store', help='')   
 
     manage = parser.add_argument_group('Run Tasks')
     manage.add_argument('-r', "--run", dest='run_single', default=0, action='count', help='Run task')    
@@ -36,7 +37,7 @@ def cli():
 
     #Handle operations
     if args.task_definition:        
-        db.add_task(db_filename = args.db_file, definition = args.task_definition, tasktype=args.task_type)
+        db.add_task(db_filename = args.db_file, definition = args.task_definition, tasktype=args.task_type, parameters=args.task_parameter)
     elif args.run_single:
         run_task(args.db_file)
     elif args.run_loop:
@@ -49,11 +50,18 @@ def run_task(db_file):
     task = db.start_task(db_file)
     if task == None:
         print("No task to run.")
-    elif task['TASKTYPE'] == 'python':
+        return
+    try:
+        parameters = tuple(task['PARAMETERS'].split(','))
+        command = task['DEFINITION'].format(*parameters)
+    except:
+        result = 'Error:', sys.exc_info()[1]
+        status='ERROR'         
+    if task['TASKTYPE'] == 'python':
         output = io.StringIO()
         with redirect_stdout(output):
             try:
-                exec(task['DEFINITION'])
+                exec(command)
                 result = output.getvalue()
                 status='COMPLETED'
             except:
@@ -63,13 +71,11 @@ def run_task(db_file):
     elif task['TASKTYPE'] == 'bash':
         output = io.StringIO()
         try:
-            result = runcmd(task['DEFINITION'])
+            result = runcmd(command)
             status='COMPLETED'
         except:
             result = 'Error:', sys.exc_info()[1]
             status='ERROR'                
-            print('x')
-            print(result)
         db.update_task(db_file, task['id'], status, result)        
 
 
